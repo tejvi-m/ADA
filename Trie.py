865ccde02f450cde5be182ed73df9349d5db2727
Tejvi M <tejvimstays@gmail.com>
2019-05-14 14:07:23 +0530

    Tries code

---
+++ Trie.py
@@ -0,0 +1,17 @@
+NodeCount = 0
+class TrieNode:
+    def __init__(self):
+        self.word = None
+        self.children = {}
+
+        global NodeCount
+        NodeCount += 1
+
+    def insert(self, word):
+        node = self
+        for letter in word:
+            if letter not in node.children:
+                node.children[letter] = TrieNode()
+
+            node = node.children[letter]
+        node.word = word
